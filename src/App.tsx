979139d15c62a3eeab52a2bbb323fc98b199ec94
olenazubashenko <olenazubashenko@gmail.com>
2020-07-06 16:47:11 +0300

    delete done

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { CardList } from './components/cardList/CardList';
+import React, { useState } from 'react';
 import { goods } from './api/goods';
 import './App.css';
+import { CardList } from './components/cardList/CardList';
 
-const goodsFromAPI = [...goods];
+//const goodsFromAPI = [...goods];
 
 function App(this: any) {
 
   //part with onDeleted will be done with useState()
-  const state = {
-  goods: goodsFromAPI
-}
+  const [_goods, setGoods] = useState(goods);
 
-
- let onDeleted = (id: any) => {
-  this.setState((prevState: { goods: any[]; }) => ({
-    goods: prevState.goods.filter((el: { id: any; }) => el.id != id)
-  }));
-};
+  let onDeleted = ( id: any ) => {
+    setGoods(
+      _goods.filter((el: { id: number; }) => el.id != id)
+    );
+  };
   return (
     <div className="App">
       <header className="App__header">
@@ -29,12 +26,12 @@ function App(this: any) {
         </div>
 
       </header>
-      <CardList 
-            goods={goodsFromAPI}
-            onGoodDeleted ={onDeleted}
-            />
+      <CardList
+        goods={_goods}
+        onGoodDeleted={onDeleted}
+      />
     </div>
   );
-}
+};
 
 export default App;
